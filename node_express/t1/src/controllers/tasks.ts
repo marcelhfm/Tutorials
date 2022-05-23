7182e3c8b4e305b067fc4d4e3d1fc1ab353400af
Marcel Hofmann <hofmann.marcel@hm.edu>
2022-05-23 21:11:43 +0200

    basic CRUD complete

--- node_express/t1/src/controllers/tasks.ts
+++ node_express/t1/src/controllers/tasks.ts
@@ -1,23 +1,70 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req: any, res: any) => {
-  res.send("all itmes form the file");
+const getAllTasks = async (req: any, res: any) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
-const createTask = (req: any, res: any) => {
-  res.json(req.body);
+const createTask = async (req: any, res: any) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
-const getTask = (req: any, res: any) => {
-  res.json({ id: req.params.id });
+const getTask = async (req: any, res: any) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findById({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
-const updateTask = (req: any, res: any) => {
-  res.send("Update Task");
+const deleteTask = async (req: any, res: any) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task: null, status: "success" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
-const deleteTask = (req: any, res: any) => {
-  res.send("delete Task");
+const updateTask = async (req: any, res: any) => {
+  try {
+    const { id: taskID } = req.params;
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 module.exports = {
--- node_express/t1/src/models/Task.ts
+++ node_express/t1/src/models/Task.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a name"],
+    trim: true,
+    maxlength: [20, "name cannot be longer than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
